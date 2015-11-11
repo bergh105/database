e25b32025995ea7aff4386f0bec84251bc5afd28
bergh105 <bergh105@mail.chapman.edu>
2015-11-10 19:25:41 -0800

    Create GenStack.h

---
+++ GenStack.h
@@ -0,0 +1,82 @@
+#ifndef GENSTACK_H_
+#define GENSTACK_H_
+#include <iostream>
+using namespace std;
+
+template<typename T>
+class GenStack {
+	public:
+		GenStack(int maxSize);
+		~GenStack();
+
+		void push(T d);
+		T pop();
+		T peak();
+
+		//methods not part of Stack interface, but helpful to have
+		bool isFull();
+		bool isEmpty();
+
+		int top;
+		int max;
+		int size;
+
+		T *myArray;
+
+};
+
+template<typename T>
+GenStack<T>::GenStack(int maxSize) {
+	myArray = new T[maxSize];
+	max = maxSize;
+	top = -1; //when we add something, it will be at 0
+	size = 0;
+}
+
+template<typename T>
+GenStack<T>::~GenStack(){
+	delete [] myArray;
+	cout << "object destroyed" << endl;
+}
+
+template<typename T>
+void GenStack<T>::push(T d) {
+	if(size != maxSize) {
+		myArray[++top] = d;
+	}
+	else {
+		for(int i=0; i<top; ++i) {
+			myArray[i] = myArray[i+1];
+		}
+		myArray[top] = d;
+	}
+	size++;
+}
+
+template<typename T>
+T GenStack<T>::pop(){
+	if(size == 0) {
+		return NULL;
+	}
+	return myArray[top--];
+}
+
+template<typename T>
+T GenStack<T>::peak() {
+	if(size == 0) {
+		return NULL;
+	}
+	return myArray[top];
+}
+
+template<typename T>
+bool GenStack<T>::isEmpty(){
+	return (top == -1);
+}
+
+template<typename T>
+bool GenStack<T>::isFull() {
+	return (top == (maxSize-1));
+}
+
+#endif /* GENSTACK_H_ */
