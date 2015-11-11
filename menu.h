78d9564328b735c6a5d46b2462a77adbbac1b0a8
titte101 <titte101@mail.chapman.edu>
2015-11-10 18:54:13 -0800

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,17 @@
+#include <iostream>
+//#include "student.h"
+//#include "faculty.h"
+
+using namespace std;
+
+class menu
+{
+	public:
+		menu();
+		~menu();
+		int input();
+		void printOptions();
+		int UserInput();
+		void execute();
+
+};
