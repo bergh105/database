6211012184d088adce481dbcd6d5df04994ff609
bergh105 <bergh105@mail.chapman.edu>
2015-11-14 20:11:35 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "Student.h"
+#include "Faculty.h"
+#include "BinaryTree.h"
+#include "Database.h"
+
+using namespace std;
+
+int main() {
+
+	cout << "Here we have the main function." << endl;
+
+	Student *s = new Student();
+
+	s->setName("Kelly");
+	s->setLevel("Sophomore");
+	s->setGPA(4.0);
+	s->setID(3);
+	s->setAdvisor(3456);
+	s->setMajor("Communications");
+
+	cout << s->getName() << endl;
+
+	Student *s2 = new Student("Adrienne", 7, 4.0, "Junior", "CIS", 45);
+
+	cout << s->operator<(s2) << endl; //this is how you have to call the operator overloader
+
+	//Database *d = new Database();
+
+	//d->AddStu();
+	
+	return 0;
+}
