dbcdce3b1edb3f76db601a752d4f4b748ae8c7f7
titte101 <titte101@mail.chapman.edu>
2015-11-17 03:20:29 -0800

    Update Database.cpp

--- Database.cpp
+++ Database.cpp
@@ -6,7 +6,7 @@
 #include "Faculty.h"
 #include "BinaryTree.h"
 
-using namespace std
+using namespace std;
 
 Database :: Database()
 {
@@ -23,7 +23,7 @@ Database :: ~Database()
 void Database :: PrintAllStu()
 {
 	// Runs through and prints students in order of ID# least to greatest
-	if (studentTable->getSize() = 0)
+	if (studentTable->getSize() == 0)
 	{
 		cout << "you dont have any students" << endl;
 	}
@@ -36,9 +36,9 @@ void Database :: PrintAllStu()
 void Database :: PrintAllFac()
 {
 	// Runs through and prints faculty in order of ID# least to greatest
-	if (facultyTable->getSize() = 0)
+	if (facultyTable->getSize() == 0)
 	{
-		cout << "This is mayhem there are no advisors"
+		cout << "This is mayhem there are no advisors" << endl;
 	}
 	else
 	{
@@ -49,18 +49,54 @@ void Database :: PrintAllFac()
 int Database :: FindStu(int stuID)
 {
 	// Finds a student by their ID #
-	s = studentTable->search(stuID);
-	cout << s << endl; //fixed operator overloaders, this should work now. if not, use next line
+	Student s; 
+	s.setID(stuID);
+	Student S = studentTable->search(s);
+	cout << S << endl; //fixed operator overloaders, this should work now. if not, use next line
 	//operator << (cout,s);
+	return 1;
 	
 }
 
 int Database :: FindFac(int facID)
 {
 	//Finds a Faculty by their ID#
-	f = facultyTable->search(facID);
-	cout << f << endl; //fixed overloader, this should work now
+	Faculty f;
+	f.setID(facID);
+	Faculty F = facultyTable->search(f);
+	cout << F << endl; //fixed overloader, this should work now
 	//operator << (cout, f);
+	return 1;
+}
+
+int Database :: FindFacByStu(int stuID)
+{
+	Student s; 
+	s.setID(stuID);
+	Student S = studentTable->search(s);
+	FindFac(S.getAdvisor());
+	
+}
+
+int Database :: FindStusByFac(int facID)
+{
+	Faculty f;
+	f.setID(facID);
+	Faculty F = facultyTable->search(f);
+	
+	int i = F.deleteFromAdviseeList();
+	if( i!=0)
+	{	
+		FindStu(i);
+	}
+	else
+	{
+		cout << "there is an issue, that faculty has no advisees" << endl;
+	}
+	while(i!=0)
+	{
+		FindStu(i);
+	}
 }
 
 int Database :: AddStu()
@@ -78,11 +114,11 @@ int Database :: AddStu()
 	getline(cin,stuName);
 	
 	cout<< "What is the student's ID number?" << endl;
-	getline(cin,holder)
+	getline(cin,holder);
 	stuID = atoi(holder.c_str());
 	
 	cout << "What is the student's GPA?" << endl;
-	getline(cin,holder)
+	getline(cin,holder);
 	stuGPA = atof(holder.c_str());
 
 	cout << "What year is the student?" << endl;
@@ -102,12 +138,18 @@ int Database :: AddStu()
 	//Rollback
 }
 
+
+
 int Database :: DeleteStu(int stuID)
 {
 	// finds a student by their ID number and deletes them from the tree
-	s = studentTable->search(stuID);
-	studentTable->remove(s);
+	Student s; 
+	s.setID(stuID);
+	Student S = studentTable->search(s);
+	studentTable->remove(S);
+	RemoveAdvisee(stuID);
 	return 1;
+	// remove student from advisor
 	//Rollback
 }
 
@@ -117,21 +159,21 @@ int Database :: AddFac()
 	string holder;
 	string facName;
 	int facID;
-	string facLevel
+	string facLevel;
 	string facDepartment;
 	DLinkedList<int>* adviseeList;
-	int adviseeID
+	int adviseeID;
 	
 
 	cout<< "What is the faculty member's name?" << endl;
 	getline(cin,facName);
 	
 	cout<< "What is the faculty member's ID number?" << endl;
-	getline(cin,holder)
+	getline(cin,holder);
 	facID = atoi(holder.c_str());
 	
 	cout << "What is the faculty member's level?" << endl;
-	getline(cin,facLevel)
+	getline(cin,facLevel);
 
 	cout << "What deparment is the faculty member in?" << endl;
 	getline(cin,facDepartment);
@@ -153,51 +195,67 @@ int Database :: AddFac()
 	//Rollback
 }
 
-int Database :: DeleteFac(int facID, advTransferID)
+int Database :: DeleteFac(int facID, int advTransferID)
 {
 	// finds a faculty by their ID # and deletes them from the tree, takes their advisees and gives
 	// them to another faculty member
-	F = facultyTable->search(facID);
+	Faculty f;
+	f.setID(facID);
+	Faculty F = facultyTable->search(f);
 	facultyTable->remove(F);
-	advTransfer = facultyTable->search(advTransferID);
-	while(F->getAdviseeListSize() != 0)
+	f.setID(advTransferID);
+	Faculty advTransfer = facultyTable->search(f);
+	while(F.getAdviseeListSize() != 0)
 	{
-		advTransfer->addToAdviseeList(F->deleteFromAdviseeList());
-	}	
-	ChangeStuAdvisor(frontID, advTransferID);
-	facultyTable->remove();
-	return 1;
-	//Rollback
-}
-
-
-int Database :: ChangeStuAdvisor(int stuID, int facID)
-{
-	//finds a student by ID, removes student from their advisor, finds a new advisor by ID,
-	//adds the student to the new advisor 
-	s = studentTable -> search(stuID);
-	newF = facultyTable -> search(facID);
-	if(s.getAdvisor() != NULL)
-	{
-		RemoveAdvisee(stuID)
+		int frontID = F.deleteFromAdviseeList()	;	
+		ChangeStuAdvisor( frontID, advTransferID);
 		
-	}
-	newF->addToAdviseeList(stuID);
-	
-	// if the student has no advisor, it skips the second step
-
+		
+	}	
+	facultyTable->remove(F);
+	return 1;
 	//Rollback
 }
 
 int Database :: RemoveAdvisee(int stuID)
 {
 	//finds a student by ID, removes them from their advisor
-	oldF = facultyTable -> search(s.getAdvisor());
-	oldF->deleteFromAdviseeList(stuID);
+	Student s; 
+	s.setID(stuID);
+	Student S = studentTable->search(s);
+	Faculty f;
+	f.setID(S.getAdvisor());
+	Faculty F = facultyTable->search(f);
+	Faculty oldF = facultyTable -> search(F);
+	oldF.deleteFromAdviseeList(stuID);
 	// is the faculty ID necessary?
 
 	//Rollback
 }
+
+int Database :: ChangeStuAdvisor(int stuID, int facID)
+{
+	//finds a student by ID, removes student from their advisor, finds a new advisor by ID,
+	//adds the student to the new advisor 
+	Student s; 
+	s.setID(stuID);
+	Student S = studentTable->search(s);
+	Faculty f;
+	f.setID(S.getAdvisor());
+	Faculty newF = facultyTable->search(f);
+	if(s.getAdvisor() != NULL)
+	{
+		RemoveAdvisee(stuID);
+		
+	}
+	newF.addToAdviseeList(stuID);
+	
+	// if the student has no advisor, it skips the second step
+
+	//Rollback
+}
+
+
 
 int Database :: Rollback()
 {
