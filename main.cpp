c86d462d78ff80719a73d6a15f50233ecafa3728
titte101 <titte101@mail.chapman.edu>
2015-11-19 02:21:04 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,39 +3,15 @@
 #include "Faculty.h"
 #include "BinaryTree.h"
 #include "Database.h"
+#include "Menu.h"
 
 using namespace std;
 
 int main() {
 
-	cout << "Here we have the main function." << endl;
-/*
-	Student *s = new Student();
-
-	s->setName("Kelly");
-	s->setLevel("Sophomore");
-	s->setGPA(4.0);
-	s->setID(3);
-	s->setAdvisor(3456);
-	s->setMajor("Communications");
-
-	cout << s->getName() << endl;
-
-	Student *s2 = new Student("Adrienne", 7, 4.0, "Junior", "CIS", 45);
-
-	cout << s->operator<(s2) << endl; // < overloader
-
-	operator<<(cout, s); // << overloader
-*/
-	Database *d = new Database();
-
-	d->AddStu();
-	d->AddStu();
-	d->AddStu();
-
-	d->PrintAllStu();
-
-	delete d;
+	
+	Menu *m = new Menu();
+	m->execute();
 	
 	return 0;
 }
