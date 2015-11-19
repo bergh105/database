ffd90ef8d0f399625d6579c998899e1524b620e7
titte101 <titte101@mail.chapman.edu>
2015-11-19 15:26:19 -0800

    this one runs the menu, other one for debug

---
+++ main1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Student.h"
+#include "Faculty.h"
+#include "BinaryTree.h"
+#include "Database.h"
+#include "Menu.h"
+
+using namespace std;
+
+int main() {
+
+	
+  Menu *m = new Menu();
+  m->execute();
+	
+	return 0;
+}
