71e7c8dabdf6326f1bf5c085e3e6b9383eaf34aa
titte101 <titte101@mail.chapman.edu>
2015-11-18 20:45:53 -0800

    small stuff

--- Database.cpp
+++ Database.cpp
@@ -308,7 +308,7 @@ void Database::Exit() {
 
 
 void Database::serializeStudents(string outFile) {
-	ofstream myfile(outFile, ios::out | ios::trunc | ios::binary);
+	ofstream myfile(outFile.c_str(), ios::out | ios::trunc | ios::binary);
 
 	if (!myfile.is_open()) {
 		cout << "Error opening file." << endl;
@@ -410,7 +410,7 @@ void Database::serializeStudents(string outFile) {
 
 BinarySearchTree<Student>* Database::deserializeStudents(string inFile) {
 	studentTable = new BinarySearchTree<Student>();
-	ifstream file(inFile, ios::in | ios::binary);
+	ifstream file(inFile.c_str(), ios::in | ios::binary);
 
 	if(!file.is_open()) {
 		cout << "error opening file." << endl;
