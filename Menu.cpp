c3e87427d1ddaa133c710578cd303a66bbcf417b
titte101 <titte101@mail.chapman.edu>
2015-11-19 02:20:14 -0800

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -2,30 +2,28 @@
 #include <stdlib.h>
 #include "Menu.h"
 #include "Database.h"
+#include "Student.h"
+#include "Faculty.h"
 
 using namespace std;
-Menu :: Menu()
-{
-	//Database d = new Database()?
-}
-Menu :: ~Menu()
-{
 
+Menu::Menu() {
+	Database d;
 }
 
+Menu :: ~Menu() {
+	
+}
 
-int Menu :: input()
-{
+int Menu::input() {
 	string userIn;
 	int i;
 	getline(cin,userIn);
-	i = atoi (userIn.c_str());
+	i = atoi(userIn.c_str());
 	return i;
-
 }
 
-void Menu :: printOptions()
-{
+void Menu::printOptions() {
 	cout << " 1. Print all student and their information " << endl;
 	cout << " 2. Print all Faculty and their information " << endl;
 	cout << " 3. Find student by ID # " << endl;
@@ -44,66 +42,73 @@ void Menu :: printOptions()
 }
 
 
-int Menu :: UserInputs()
-{
+int Menu::UserInputs() {
+	printOptions();
 	cout << "choose an action from the above list" << endl;
 	int i = input();
 	if (i < 15 && i>0)
 		return i;
-	else
-	{
+	else {
 		UserInputs();
 	}	
 
 }
 
-void Menu :: execute()
-{
-
+void Menu::execute() {
 	// each case refers to a different action, call appropriate methods
+	
+	
 	int i = UserInputs();
-	switch(i)
-	{
+	switch(i) {
 		case 1:
-		{//print all students
+		{ //print all students
 			d.PrintAllStu();
 			break;
 		}
-		case 2:
-		{//print all fac
+		case 2: //print all fac
+		{			
 			d.PrintAllFac();
+			execute();
 			break;
-		}
-		case 3:
+		}		
+
+		case 3: //find stu
 		{
-		//find stu
 			cout<< "What is the ID number of the student you are looking for?" << endl;
 			getline(cin,holder);
-			d.FindStu(atoi(holder.c_str()));
+			int i = atoi(holder.c_str());
+			d.FindStu(i);
+			execute();
 			break;
 		}
 		case 4:
 		{
 			cout<< "What is the ID number of the faculty you are looking for?" << endl;
 			getline(cin,holder);
-			d.FindFac(atoi(holder.c_str()));
+			int i = atoi(holder.c_str());
+			d.FindFac(i);
+			execute();
 			break;
 		}
 		case 5:
 		{
 			cout<<"What is the student's ID number?"<<endl;
 			getline(cin,holder);
-			d.FindFacByStu(atoi(holder.c_str()));
+			int i = atoi(holder.c_str());
+			d.FindFacByStu(i);
+			execute();
 			break;
 		}
 		case 6:
 		{
 			cout << "What is the Faculty's ID number?" << endl;
 			getline(cin,holder);
-			d.FindStusByFac(atoi(holder.c_str()));
+			int i = atoi(holder.c_str());
+			d.FindStusByFac(i);
+			execute();
 			break;
 		}
-		case 7:
+		case 7:	
 		{
 			d.AddStu();
 			break;
@@ -112,12 +117,15 @@ void Menu :: execute()
 		{
 			cout << "What is the student's ID number?" << endl;
 			getline(cin,holder);
-			d.DeleteStu(atoi(holder.c_str()));
+			int i = atoi(holder.c_str());
+			d.DeleteStu(i);
+			execute();
 			break;
 		}
 		case 9:
 		{
 			d.AddFac();
+			execute();
 			break;
 		}
 		case 10:
@@ -125,9 +133,12 @@ void Menu :: execute()
 			cout << "What is the ID number of the faculty?" << endl;
 			string advTransfer;
 			getline(cin,holder);
+			int i = atoi(holder.c_str());
 			cout << "What is the ID number of the faculty who is taking on the advisees?" << endl;
 			getline(cin,advTransfer);
-			d.DeleteFac(atoi(holder.c_str()),atoi(advTransfer.c_str()));
+			int t = atoi(advTransfer.c_str());
+			d.DeleteFac(i,t);
+			execute();
 			break;
 		}
 		case 11:
@@ -135,21 +146,27 @@ void Menu :: execute()
 			string facID;
 			cout << "What is the ID number of the student?" << endl;
 			getline(cin,holder);
+			int i = atoi(holder.c_str());
 			cout << "What is the ID number of their new advisor?" << endl;
 			getline(cin,facID);
-			d.ChangeStuAdvisor(atoi(holder.c_str()),atoi(facID.c_str()));
+			int t = atoi(facID.c_str());
+			d.ChangeStuAdvisor(i,t);
+			execute();
 			break;
 		}
-		case 12:
+		case 12:	
 		{
 			cout << "What is the ID number of the student?" << endl;	
 			getline(cin,holder);
-			d.RemoveAdvisee(atoi(holder.c_str()));
+			int i = atoi(holder.c_str());
+			d.RemoveAdvisee(i);
+			execute();
 			break;
 		}
 		case 13:
 		{
 			d.Rollback();
+			execute();
 			break;
 		}
 		case 14:
@@ -159,18 +176,10 @@ void Menu :: execute()
 		}
 		default:
 		{
-			execute();
-			break;		
+			execute();		
+			break;
 		}
 	}
 
 }
-
-
-
-
-
-
-
-
 
