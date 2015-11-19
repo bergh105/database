39f834b298908c741b8f2eca1d1f66b6d6e0a0c3
titte101 <titte101@mail.chapman.edu>
2015-11-19 02:19:00 -0800

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -1,26 +1,25 @@
-#ifndef DATABASE_H_
-#define DATABASE_H_
+#ifndef MENU_H_
+#define MENU_H_
+
 #include <iostream>
 #include "Student.h"
 #include "Faculty.h"
+#include "BinaryTree.h"
 #include "Database.h"
-
 
 using namespace std;
 
-class Menu
-{
-	public:
-		Menu();
-		~Menu();
-		int input();
-		void printOptions();
-		int UserInputs();
-		void execute();
-		Database d;
-		string holder;
+class Menu{
+public:
+	Menu();
+	~Menu();
+	int input();
+	void printOptions();
+	int UserInputs();
+	void execute();
+	string holder;
+	Database d;
 
 };
-
 
 #endif
